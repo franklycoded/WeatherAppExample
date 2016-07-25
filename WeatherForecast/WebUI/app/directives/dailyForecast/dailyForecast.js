cc4edbc8cc0340b499f517a08d9dca70545b8727
Peter Frank <peter.frank@hotmail.co.uk>
2016-07-26 00:28:13 +0100

    Connected ui and backend

---
+++ WeatherForecast/WebUI/app/directives/dailyForecast/dailyForecast.js
@@ -0,0 +1,11 @@
+﻿var weatherApp = angular.module('WeatherApp');
+
+weatherApp.directive('dailyForecast', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            forecast: '=forecast'
+        },
+        templateUrl: '/WebUI/app/directives/dailyForecast/dailyForecast.html'
+    };
+});
--- WeatherForecast/WebUI/app/weatherMain/weatherMain.js
+++ WeatherForecast/WebUI/app/weatherMain/weatherMain.js
@@ -1,12 +1,15 @@
 ﻿var weatherApp = angular.module('WeatherApp');
+
 weatherApp.controller('WeatherMainController', ['$scope', 'forecastService', function ($scope, forecastService) {
     $scope.cityName = "";
+    $scope.weatherData = {};
     $scope.getForecast = function () {
         console.log("get forecast for " + $scope.cityName);
 
         forecastService.getForecast($scope.cityName)
-            .then(function (data) {
-                console.log(data);
+            .then(function (weatherData) {
+                console.log(weatherData);
+                $scope.weatherData = weatherData.data;
             });
     }
 }]);
