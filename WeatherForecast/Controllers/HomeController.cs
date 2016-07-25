f4c8466e9651a332faafd38436e0f4f2a9326793
Peter Frank <peter.frank@hotmail.co.uk>
2016-07-25 21:19:11 +0100

    Modified home template Added webui folder structure

--- WeatherForecast/Controllers/HomeController.cs
+++ WeatherForecast/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Web.Mvc;
 
 namespace WeatherForecast.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
---
+++ WeatherForecast/WebUI/app/app.js
@@ -0,0 +1,12 @@
+﻿angular.module('WeatherApp', [])
+    //.controller('QuizCtrl', function ($scope, $http) {
+    //    $scope.answered = false;
+    //    $scope.title = "loading question...";
+    //    $scope.options = [];
+    //    $scope.correctAnswer = false;
+    //    $scope.working = false;
+
+    //    $scope.answer = function () {
+    //        return $scope.correctAnswer ? 'correct' : 'incorrect';
+    //    };
+    //});
